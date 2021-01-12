400de27a6e150aed26a5e93e9836e1ae387bffb8
Paul Hadfield <paul@phad.org.uk>
2021-01-12 12:46:00 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <h2>React beginnings...</h2>
+    </div>
+  );
+}
